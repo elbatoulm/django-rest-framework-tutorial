ae4bb2abc0c373885b751512a75f0fc1dc6b7b02
elbatoulmaz <fe_mazighi@esi.dz>
2020-08-27 16:12:42 +0100

    used rest_framework routers to route the views instead of hardcoding everything myself

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,39 +1,57 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from snippets.views import SnippetViewSet, UserViewSet, api_root
 from rest_framework import renderers
+from rest_framework.routers import DefaultRouter
 
-snippet_list = SnippetViewSet.as_view(
-    {
-        'get': 'list',
-        'post': 'create',
-    }
-)
+router = DefaultRouter()
+router.register(r'snippets', SnippetViewSet)
+router.register(r'users', UserViewSet)
 
-snippet_detail = SnippetViewSet.as_view(
-    {
-        'get': 'retrieve',
-        'put': 'update',
-        'patch': 'partial_update',
-        'delete': 'destory',
-    }
-)
+urlpatterns = [
+    path('', include(router.urls))
+]
+# snippet_list = SnippetViewSet.as_view(
+#     {
+#         'get': 'list',
+#         'post': 'create',
+#     }
+# )
 
-snippet_highlight = SnippetViewSet.as_view(
-    {
-        'get': 'highlight',
-    }, renderer_classes=[renderers.StaticHTMLRenderer]
-)
+# snippet_detail = SnippetViewSet.as_view(
+#     {
+#         'get': 'retrieve',
+#         'put': 'update',
+#         'patch': 'partial_update',
+#         'delete': 'destory',
+#     }
+# )
 
-user_list = UserViewSet.as_view(
-    {
-        'get': 'list',
-    }
-)
+# snippet_highlight = SnippetViewSet.as_view(
+#     {
+#         'get': 'highlight',
+#     }, renderer_classes=[renderers.StaticHTMLRenderer]
+# )
 
-user_detail = UserViewSet.as_view({
-    'get': 'retrieve',
-})
+# user_list = UserViewSet.as_view(
+#     {
+#         'get': 'list',
+#     }
+# )
+
+# user_detail = UserViewSet.as_view({
+#     'get': 'retrieve',
+# })
+
+# urlpatterns = format_suffix_patterns([
+#     path('', api_root),
+#     path('snippets/', snippet_list, name='snippet-list'),
+#     path('snippets/<int:pk>/', snippet_detail, name='snippet-detail'),
+#     path('snippets/<int:pk>/highlight',
+#          snippet_highlight, name='snippet-highlight'),
+#     path('users/', user_list, name='user-list'),
+#     path('users/<int:pk>/', user_detail, name='user-detail'),
+# ])
 
 # urlpatterns = [
 #     path('snippets/', views.SnippetList.as_view(), name='snippet-list'),
@@ -44,12 +62,3 @@ user_detail = UserViewSet.as_view({
 #     path('snippets/<int:pk>/highlight/',
 #          views.SnippetHighlight.as_view(), name='snippet-highlight'),
 # ]
-urlpatterns = format_suffix_patterns([
-    path('', api_root),
-    path('snippets/', snippet_list, name='snippet-list'),
-    path('snippets/<int:pk>/', snippet_detail, name='snippet-detail'),
-    path('snippets/<int:pk>/highlight',
-         snippet_highlight, name='snippet-highlight'),
-    path('users/', user_list, name='user-list'),
-    path('users/<int:pk>/', user_detail, name='user-detail'),
-])
--- snippets/views.py
+++ snippets/views.py
@@ -26,7 +26,7 @@ class SnippetViewSet(viewsets.ModelViewSet):
     permission_classes = [
         permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
-    @action(detail=True, rederer_classes=[renderers.StaticHTMLRenderer])
+    @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
     def highlight(self, request, *args, **kwargs):
         snippet = self.get_object()
         return Response(snippet.highlighted)

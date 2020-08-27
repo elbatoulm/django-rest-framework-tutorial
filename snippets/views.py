1263100712fc02d1fbfef00279e9eba06b04b172
elbatoulmaz <fe_mazighi@esi.dz>
2020-08-27 13:15:11 +0100

    related snippets to an owner (a user) + added authentification of users and restricted permission to editing snippets by the user who created them only

--- snippets/views.py
+++ snippets/views.py
@@ -1,17 +1,38 @@
 #####Implemneted the views class-based using generics########
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from django.contrib.auth.models import User
+from snippets.serializers import SnippetSerializer, UserSerializer
 from rest_framework import generics
+from rest_framework import permissions
+from snippets.permissions import IsOwnerOrReadOnly
 
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    # added to restrict permissions on who can read
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    # added to restrict persmission on who can update, destroy
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 
 #####Implemneted the views using Mixins########

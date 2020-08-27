09dbfef2752d68cc2658853aa6ec60679f0d3227
elbatoulmaz <fe_mazighi@esi.dz>
2020-08-27 13:38:21 +0100

    added a view for the highlight of snippets

--- snippets/views.py
+++ snippets/views.py
@@ -5,6 +5,10 @@ from snippets.serializers import SnippetSerializer, UserSerializer
 from rest_framework import generics
 from rest_framework import permissions
 from snippets.permissions import IsOwnerOrReadOnly
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework.decorators import api_view
+from rest_framework import renderers
 
 
 class SnippetList(generics.ListCreateAPIView):
@@ -15,6 +19,14 @@ class SnippetList(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
@@ -33,6 +45,15 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
 
 #####Implemneted the views using Mixins########
